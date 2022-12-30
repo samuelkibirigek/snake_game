39501c208022711f7232a0243260699fb16f458f
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-30 14:44:55 +0300

    The scoreboard file

--- food.py
+++ food.py
@@ -9,6 +9,10 @@ class Food(Turtle):
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("red")
+        self.refresh()
+
+    def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
+
